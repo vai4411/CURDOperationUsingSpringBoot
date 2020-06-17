8664569bfe29039f8ed12393f041fc68c8388308
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-06-18 02:33:24 +0530

    ADDED: Created Database Connection Provider

---
+++ src/main/java/com/bl/demo/curdoperation/utility/ConnectionProvider.java
@@ -0,0 +1,8 @@
+package com.bl.demo.curdoperation.utility;
+
+public interface ConnectionProvider {
+    String username="Vaibhav";
+    String password ="Vaibhav@123";
+    String connURL="jdbc:mysql://localhost:3306/register";
+
+}
